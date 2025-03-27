bf5b47ce11763151f91ddc7f104d1488f3595521
PettHa <127201156+PettHa@users.noreply.github.com>
2025-03-27 09:20:25 +0100

    Header fikset. Må lenkes

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -3,19 +3,22 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="left-container-header">
-        <h1 className="header-title">BS<span className="exclamation">!</span>KKERHET</h1>
-      </div>
-      <div className='btn-container'>
-        <button className='header-btn'>Start</button>
-        <button className='header-btn'>Test</button>
-        <button className='header-btn'>Resultat</button>
-      </div>
-      <div className="vikings-container">
-        <div style={{ fontSize: '40px' }}>👨‍🦰👨‍🦱👨‍🦳</div>
-      </div>
-    </header>
+    <>
+      <header className="header">
+        <div className="left-container-header">
+          <h1 className="header-title">BS<span className="exclamation">!</span>KKERHET</h1>
+        </div>
+        <div className='btn-container'>
+          <button className='header-btn'><p>Hjem</p></button> 
+          <button className='header-btn'><p>Test</p></button>
+          <button className='header-btn'><p>Resultat</p></button>
+        </div>
+        <div className="vikings-container">
+          <div className='vikings'>👨‍🦰👨‍🦱👨‍🦳💂‍♂️</div>
+        </div>
+      </header>
+      <div className='backround-header'></div>
+    </>
   );
 };
 

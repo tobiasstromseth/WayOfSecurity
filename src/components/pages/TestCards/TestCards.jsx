a58cabe5a3b81445ec7d93cb2f4f26d9c2fdbeef
PettHa <127201156+PettHa@users.noreply.github.com>
2025-03-29 18:53:23 +0100

    forsøker å rydde opp i kortfactory

--- src/components/pages/TestCards/TestCards.jsx
+++ src/components/pages/TestCards/TestCards.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import Header from '../../common/Header/Header';
-import CardContainer from '../../common/Card/CardContainer';
+import CardFactory from '../../common/Card/CardFactory';
 import './TestCards.css';
 
 const TestCards = () => {
   return (
     <div className="page-container">
-      <Header />
-      
-      <header className="cards-header">
-        <h1 className="cards-title">SIKKERHETSKORT</h1>
-        <p className="cards-subtitle">Utforsk sikkerhetstiltak for din bedrift</p>
-      </header>
-      
-      <CardContainer />
+      <Header />      
+
+      <CardFactory />
     </div>
   );
 };
